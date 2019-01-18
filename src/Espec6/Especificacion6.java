a6a6ce623c61739addc21943c7de7f4e20e5e16e
APachon10 <Pachondestroy95@gmail.com>
2019-01-18 20:01:02 +0100

    Estructura de la ventana Hecha

--- src/Espec6/Especificacion6.java
+++ src/Espec6/Especificacion6.java
@@ -18,10 +18,13 @@ import javax.swing.JButton;
 import javax.swing.JDesktopPane;
 import java.awt.Color;
 import javax.swing.JMenu;
+import javax.swing.JTextField;
 
 public class Especificacion6 extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
 	public Especificacion6() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 788, 529);
@@ -44,23 +47,75 @@ public class Especificacion6 extends JFrame {
 					.addContainerGap())
 		);
 		
-		JInternalFrame internalFrame = new JInternalFrame("New JInternalFrame");
+		JInternalFrame internalFrame = new JInternalFrame("Login");
+		internalFrame.setResizable(true);
+		internalFrame.setClosable(true);
 		internalFrame.setVisible(true);
 		GroupLayout gl_Panel = new GroupLayout(Panel);
 		gl_Panel.setHorizontalGroup(
 			gl_Panel.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_Panel.createSequentialGroup()
-					.addGap(164)
-					.addComponent(internalFrame, GroupLayout.PREFERRED_SIZE, 422, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(166, Short.MAX_VALUE))
+					.addGap(225)
+					.addComponent(internalFrame, GroupLayout.PREFERRED_SIZE, 266, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(261, Short.MAX_VALUE))
 		);
 		gl_Panel.setVerticalGroup(
 			gl_Panel.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_Panel.createSequentialGroup()
-					.addGap(146)
+				.addGroup(Alignment.TRAILING, gl_Panel.createSequentialGroup()
+					.addContainerGap(142, Short.MAX_VALUE)
 					.addComponent(internalFrame, GroupLayout.PREFERRED_SIZE, 165, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(158, Short.MAX_VALUE))
+					.addGap(138))
 		);
+		
+		JLabel lblLogin = new JLabel("Login");
+		
+		JLabel lblPassword = new JLabel("Password");
+		
+		JButton btnEnviar = new JButton("Enviar");
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		GroupLayout groupLayout_1 = new GroupLayout(internalFrame.getContentPane());
+		groupLayout_1.setHorizontalGroup(
+			groupLayout_1.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout_1.createSequentialGroup()
+					.addGroup(groupLayout_1.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout_1.createSequentialGroup()
+							.addGap(52)
+							.addGroup(groupLayout_1.createParallelGroup(Alignment.LEADING, false)
+								.addGroup(groupLayout_1.createSequentialGroup()
+									.addPreferredGap(ComponentPlacement.RELATED)
+									.addComponent(lblPassword)
+									.addGap(18)
+									.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+								.addGroup(groupLayout_1.createSequentialGroup()
+									.addComponent(lblLogin)
+									.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+									.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))))
+						.addGroup(groupLayout_1.createSequentialGroup()
+							.addGap(89)
+							.addComponent(btnEnviar)))
+					.addContainerGap(212, Short.MAX_VALUE))
+		);
+		groupLayout_1.setVerticalGroup(
+			groupLayout_1.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout_1.createSequentialGroup()
+					.addGap(35)
+					.addGroup(groupLayout_1.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblLogin)
+						.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(groupLayout_1.createParallelGroup(Alignment.BASELINE)
+						.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblPassword))
+					.addGap(11)
+					.addComponent(btnEnviar)
+					.addContainerGap())
+		);
+		internalFrame.getContentPane().setLayout(groupLayout_1);
 		Panel.setLayout(gl_Panel);
 		getContentPane().setLayout(groupLayout);
 		
